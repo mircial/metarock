e8d7b93dd3f3af048628de0c4a760c5a899baaa0
CodeAunt <zhouzili98@hotmail.com>
2021-09-19 16:34:55 +0800

    web3

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,59 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./Header.module.css";
 import { GlobalOutlined } from "@ant-design/icons";
 import { useHistory, useLocation, useParams, useRouteMatch } from "react-router-dom";
-
+import Web3 from 'web3';
 // import logo from "../../assets/logo.svg";
+declare let window: any;
 
+// nothing
 export const Header: React.FC = () => {
   const history = useHistory();
   const location = useLocation();
   const params = useParams();
   const match = useRouteMatch();
-  const address = "0xbC108A8Cc826784dC2F98003392180d1ac9EAa9b"
+  var address = ""
+  var web3;
 
-  const addressText = address.substring(0, 10)+"..."
+  const [add, setAdd] = useState("Connect Wallet");
 
   const ethEnabled = async () => {
-    // if (typeof window.ethereum !== 'undefined') {
-    //     // Instance web3 with the provided information from the MetaMask provider information
-    //     this.web3 = new Web3(window.ethereum);
-    //     try {
-    //         // Request account access
-    //         await window.ethereum.enable();
-    //         return true
-    //     } catch (e) {
-    //         // User denied access
-    //         return false
-    //     }
-    // }
+    if (typeof window.ethereum !== 'undefined') {
+        // Instance web3 with the provided information from the MetaMask provider information
+        web3 = new Web3(window.ethereum);
+        try {
+            // Request account access
+            await window.ethereum.enable();
+            return true
+        } catch (e) {
+            // User denied access
+            return false
+        }
+    }
     return false;
   }
 
+  const clickConnect = async () => {
+    // this.setState({step: 3});  // to confirm page..
+    console.log(localStorage.getItem("address"))
+    if (!await ethEnabled()) {
+        alert("How dare you~");
+    }
+    address = await web3.eth.getAccounts(); 
+    if(address.length>0){
+      localStorage.setItem("address",address[0])
+      setAdd(address[0].substring(0, 10)+"...")
+    }
+    // const balance = await web3.eth.getBalance(this.address);
+    // console.log('blance: ' + balance/1e18);
+      
+  }
+
+  if( add=="Connect Wallet" && localStorage.getItem("address") != undefined){
+    console.log('new')
+    setAdd((localStorage.getItem("address") as string).substring(0, 10)+"...")
+  }
+  
   return (
     <div className={styles["header"]}>
       <div className={styles["header-main"]}>
@@ -45,11 +69,10 @@ export const Header: React.FC = () => {
             <div className={styles["menu-item"]} onClick={()=>history.push("/")}>Home</div>
             <div className={styles["menu-item"]} onClick={()=>history.push("/create")}>Create</div>
             <div className={styles["menu-item"]} onClick={()=>history.push("/evolve")}>Evolve</div>
-            {/* <div className={styles["menu-item"]} onClick={()=>history.push("/evolve")}>Connect</div> */}
           </span>
 
-          <div className={styles["address-block"]}>
-            { addressText }
+          <div className={styles["address-block"]} onClick={clickConnect} >
+            { add }
           </div>
         </div>
       </div>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 
 import styles from "./HomePage.module.css";
+import { useHistory } from "react-router-dom";
 import {Header } from "../../components";
 
-export class HomePage extends React.Component {
-  render() {
-    console.log(this.props)
-    return (
-      <>
-        <Header />
-        {/* 页面内容 content */}
-        <div className={styles["page-content"]}
-          style={{ 
-            backgroundImage: `url("./homebg.jpg")`, 
-            backgroundRepeat: 'no-repeat', 
-            backgroundSize: 'contain',
-            backgroundPosition: 'center center' }}>
-        </div>
-      </>
-    );
-  }
+export const HomePage: React.FC = () => {
+  const history = useHistory();
+  return (
+    <>
+      <Header />
+      {/* 页面内容 content */}
+      <div className={styles["page-content"]}
+        style={{ 
+          backgroundImage: `url("./homebg.jpg")`, 
+          backgroundRepeat: 'no-repeat', 
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center' }}>
+            <img className={styles["create-now"]} onClick={()=>history.push("/create")} src='./create-now.png'/>
+            <img className={styles["big-circle"]}  src='./rock-circle.png'></img>
+            <div className={styles["bottom"]}> 
+              <span>
+                learn more
+              </span>
+              <div className={styles["bottom-link"]}>
+                <span onClick={()=>history.push("https://opensea.io/collection/metarocks")}>
+                  Opensea <img src='./Arrow.png'/></span>
+                <span onClick={()=>history.push("https://rarible.com")}>
+                  Rarible <img src='./Arrow.png'/></span> 
+                <span onClick={()=>history.push("https://etherscan.io/")} >
+                  Etherscan <img src='./Arrow.png'/></span> 
+              </div>
+            </div>
+      </div>
+    </>
+  );
 }
+
+//
